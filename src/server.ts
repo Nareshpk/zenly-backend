43e08bfc87b8bdf8e4c671e4a9d787a7aa075a33
nareshpk <nareshpk607@gmail.com>
2025-12-07 23:05:26 +0530

    Refactor server.ts: simplify code structure and remove unused imports

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,11 @@
-import express from "express";
-import cors from "cors";
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
 
-import connectDB from "./config/db";
-import path from "path";
-import authRoutes from "./routes/authRoutes";
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World!')
+})
 
-const app = express();
-const port = process.env.PORT || 18012;
-
-const allowedOrigins = ["https://zenly-admin.onrender.com", "http://localhost:3000"];
-
-app.use(((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const origin = req.headers.origin as string;
-
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-}) as express.RequestHandler);
-;
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-
-connectDB().then(() => {
-  app.listen(port, () => console.log(`Server running on port ${port}`));
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
